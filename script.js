8f7ae170b81ecebd94f186e0a027f644a757ffe1
feet <weldersilvasurf@gmail.com>
2022-06-14 20:02:15 -0300

    repetiçaõ

---
+++ script.js
@@ -0,0 +1,29 @@
+for(let num = 1; num <=10; num+=1){
+    console.log(num)
+}
+
+let num = 10;
+while (num >=1 ){
+    console.log(num)
+    num-=1
+}
+
+    
+let number = 1;
+while (number <=100){  
+        console.log(number)
+    number+=2
+}
+
+for(let number = 0; number <=100; number+=2){
+    console.log(number)
+}
+
+
+
+
+
+
+
+
+
